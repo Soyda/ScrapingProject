72ab05657e835b4d0d2ee9589e5e814fd9e056ab
Steven <stevendruesne@gmail.com>
2021-12-27 15:40:22 +0100

    Forgot to push

--- animalsScraping/app.py
+++ animalsScraping/app.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Animals")
+animal_df = pd.read_json('not_really_clean_animals.json')
+
+st.title("Animals")
+st.write(animal_df)
